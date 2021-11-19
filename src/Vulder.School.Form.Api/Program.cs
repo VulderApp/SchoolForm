7438e2a7db0f613fa886a332c7455f52196b7b6e
MRmlik12 <mrmlik12sup@gmail.com>
2021-11-19 20:10:22 +0100

    Add default CORS policy

--- src/Vulder.School.Form.Api/Program.cs
+++ src/Vulder.School.Form.Api/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddValidators();
+builder.Services.AddDefaultCorsPolicy();
 
 var app = builder.Build();
 
