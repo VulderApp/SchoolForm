ff38162eac35c0423b9e1a6829b4170cd47b8834
Daniel Olczyk <44818681+MRmlik12@users.noreply.github.com>
2022-01-19 21:06:37 +0100

    Add get school form endpoint (#6)

---
+++ src/Vulder.School.Form.Application/Form/GetSchoolForm/GetSchoolFormRequestHandler.cs
@@ -0,0 +1,20 @@
+using MediatR;
+using Vulder.School.Form.Core.Models;
+using Vulder.School.Form.Infrastructure.Database.Interfaces;
+
+namespace Vulder.School.Form.Application.Form.GetSchoolForm;
+
+public class GetSchoolFormRequestHandler : IRequestHandler<SchoolFormGetModel, Core.ProjectAggregate.Form.Form>
+{
+    private readonly IFormRepository _formRepository;
+    
+    public GetSchoolFormRequestHandler(IFormRepository formRepository)
+    {
+        _formRepository = formRepository;
+    }
+    
+    public Task<Core.ProjectAggregate.Form.Form> Handle(SchoolFormGetModel request, CancellationToken cancellationToken)
+    {
+        return _formRepository.GetById(request.Id);
+    }
+}
