42294c4391b8b75a96b19dac6c93f25dcdfeaef4
Daniel Olczyk <44818681+MRmlik12@users.noreply.github.com>
2022-01-21 11:29:39 +0100

    Add refuse school form endpoint (#7)

--- src/Vulder.School.Form.Core/ProjectAggregate/Form/Form.cs
+++ src/Vulder.School.Form.Core/ProjectAggregate/Form/Form.cs
@@ -24,13 +24,23 @@ public class Form : BaseEntity, IRequest<Form>
 
     [BsonRequired]
     public bool Approved { get; set; }
+    
+    [BsonRequired]
+    public bool Refused { get; set; }
 
     [BsonRequired]
     [BsonRepresentation(BsonType.String)]
     public Guid? ApproveAdminId { get; set; }
+    
+    [BsonRequired]
+    [BsonRepresentation(BsonType.String)]
+    public Guid? RefusedAdminId { get; set; }
 
     [BsonRequired]
     public DateTime? ApprovedAt { get; set; }
+    
+    [BsonRequired]
+    public DateTime? RefusedAt { get; set; }
 
     public Form GenerateId()
     {
@@ -49,6 +59,15 @@ public class Form : BaseEntity, IRequest<Form>
     public Form SetApproveTime()
     {
         ApprovedAt = DateTime.UtcNow;
+
+        return this;
+    }
+    
+    public Form SetRefuse(Guid adminId)
+    {
+        Refused = true;
+        RefusedAdminId = adminId;
+        RefusedAt = DateTime.UtcNow;
 
         return this;
     }
