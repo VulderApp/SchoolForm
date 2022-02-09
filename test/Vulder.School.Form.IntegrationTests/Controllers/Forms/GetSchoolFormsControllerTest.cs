014fdd28fe973c1c211982c88c1935c3ceb40b54
Daniel Olczyk <44818681+MRmlik12@users.noreply.github.com>
2022-02-09 20:21:32 +0100

    Add GetSchoolForms controller test (#16)

---
+++ test/Vulder.School.Form.IntegrationTests/Controllers/Forms/GetSchoolFormsControllerTest.cs
@@ -0,0 +1,34 @@
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Vulder.School.Form.Core.Models;
+using Vulder.School.Form.IntegrationTests.Fixtures;
+using Xunit;
+
+namespace Vulder.School.Form.IntegrationTests.Controllers.Forms;
+
+public class GetSchoolFormsController
+{
+    [Fact]
+    public async Task POST_Responds_200_StatusCode()
+    {
+        var formModel = new SchoolFormModel
+        {
+            Email = "example@example.com",
+            SchoolName = "ZSP 3 w Warszawie",
+            SchoolUrl = "https://example.com",
+            TimetableUrl = "https://example.com/timetable"
+        };
+
+        await using var application = new WebAppFactoryFixture();
+        using var client = application.CreateClient();
+        var httpContent = new StringContent(JsonConvert.SerializeObject(formModel), Encoding.UTF8, "application/json");
+        await client.PostAsync("/form/SubmitSchoolForm", httpContent);
+
+        using var response = await client.GetAsync($"/form/GetSchoolForms?page=1");
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+}
