e5e77cc0d25f7cecc3e5458bd984cd1490cbbf69
MRmlik12 <mrmlik12sup@gmail.com>
2022-04-10 20:24:40 +0200

    Add logger to log exceptions and traffic

--- src/Vulder.School.Form.Api/Program.cs
+++ src/Vulder.School.Form.Api/Program.cs
@@ -3,14 +3,10 @@ using Autofac.Extensions.DependencyInjection;
 using Vulder.School.Form.Application;
 using Vulder.School.Form.Infrastructure;
 using Vulder.SharedKernel;
+using NLog.Web;
+using Vulder.SharedKernel.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory(containerBuilder =>
-{
-    containerBuilder.RegisterModule(new DefaultInfrastructureModule());
-    containerBuilder.RegisterModule(new DefaultApplicationModule());
-}));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -19,6 +15,13 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddValidators();
 builder.Services.AddDefaultCorsPolicy();
 
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory(containerBuilder =>
+{
+    containerBuilder.RegisterModule(new DefaultInfrastructureModule());
+    containerBuilder.RegisterModule(new DefaultApplicationModule());
+}));
+builder.Host.UseNLog();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -26,6 +29,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<GlobalExceptionHandlerMiddleware>();
+app.UseMiddleware<ControllerActionLoggingMiddleware>();
 
 app.UseCors("CORS");
 
